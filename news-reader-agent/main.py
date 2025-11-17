1220657ec051293e526365612d82cdef4d06ff60
thinkerdoun <thinkerdoun@gmail.com>
2025-11-17 13:13:21 +0900

    commit my first ai agent

---
+++ news-reader-agent/main.py
@@ -0,0 +1,57 @@
+import dotenv
+
+dotenv.load_dotenv()
+
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+from tools import count_letters
+
+@CrewBase
+class TranslatorCrew:
+
+    @agent
+    def translator_agent(self):
+        return Agent(
+            config=self.agents_config["translator_agent"],
+        )
+
+    @agent
+    def counter_agent(self):
+        return Agent(
+            config=self.agents_config["counter_agent"],
+            tools=[count_letters],
+        )
+
+
+    @task
+    def translate_task(self):
+        return Task(
+            config=self.tasks_config["translate_task"],
+        )
+    
+    @task
+    def retranslate_task(self):
+        return Task(
+            config=self.tasks_config["retranslate_task"],
+        )
+
+    @task
+    def count_task(self):
+        return Task(
+            config=self.tasks_config["count_task"],
+            tools=[count_letters],
+        )
+    
+    @crew
+    def assemble_crew(self):
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            verbose=True,
+        )
+
+TranslatorCrew().assemble_crew().kickoff(
+    inputs={
+        "sentence": "Hello my name is doun Kwon.",
+    }
+)
